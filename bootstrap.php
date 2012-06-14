6396c77f7bd6d3963a741c85dda4aa99037bc801
David Haskins <davidhaskins@csgholdingsllc.com>
2012-06-14 08:58:24 -0500

    fixed user,password,db_name TODO: remove references to dingo

--- bootstrap.php
+++ bootstrap.php
@@ -25,11 +25,11 @@ $db = $doc_root.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'DB.php';
 require_once('DB.php');
 
 //DSN for CSG_Dingo aka "dsn2"
-$user = 'hfrg';
-$pass = '*****';
+$user = 'experiment';
+$pass = 'experimentpassword';
 $host = $_SERVER["COMPUTERNAME"] . '\SQLEXPRESS';
 
-$db_name = 'CSG_Dingo';
+$db_name = 'Experiment';
 $dsn_dingo = "mssql://$user:$pass@$host/$db_name";
 
 define('DSN_DINGO',$dsn_dingo);
